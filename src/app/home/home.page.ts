7fbad03494c4be5c7e278b8c43e3fdfc07465861
Pedro Lima <psantoslima86@gmail.com>
2022-03-09 22:41:48 -0300

    Pedro
    Realizado função de sorteio, adicionado imagem do dado abaixo do número

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BlockingProxy } from 'blocking-proxy';
+import { delay } from 'q';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,19 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  dado: number = 0;
+  sorteando:boolean = false;
+
   constructor() {}
 
+  async sortear() {
+    for (let i = 0; i < 10; i++) {
+      this.sorteando = true;
+      let numSorteado = Math.random();
+      this.dado = Math.floor(numSorteado * 6 + 1);
+      await new Promise(f => setTimeout(f, 200));
+      console.log(this.dado);
+    }
+    this.sorteando = false;
+  }
 }
